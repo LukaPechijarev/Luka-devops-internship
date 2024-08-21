dce0f6d90a9d8a0c74167bf72384fff0f56d5a3e
LukaPechijarev <luka.pechijarev@scalefocus.com>
2024-08-21 21:47:42 +0200

    clear

--- Week3project/simpleapp/app.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req,res) => res.send('Hello World!Test na porta 9900'))
-app.listen(9900, () => console.log('Server ready'))
